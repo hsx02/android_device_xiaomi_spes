2cd464cc0d73bb217838a20724f524e2e8722827
hsx02 <hsx02self@gmail.com>
2022-07-01 06:47:31 +0000

    spes: bringup to AwakenOS!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The AwakenOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spes.mk
+    $(LOCAL_DIR)/awaken_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_spes-user \
-    lineage_spes-userdebug \
-    lineage_spes-eng
+    awaken_spes-user \
+    awaken_spes-userdebug \
+    awaken_spes-eng
